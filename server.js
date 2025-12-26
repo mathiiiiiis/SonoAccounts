19fa7cd090328ba04a836ff6d23b4d216390641c
mathiiiiiis <mathis@mathiiis.de>
2025-12-26 01:26:17 +0100

    fix(server): im a fucking idiot

--- server.js
+++ server.js
@@ -39,7 +39,17 @@ app.use((req, res, next) => {
 
 //cors headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://web.sono.wtf', 'https://sono.wtf');
+  const allowedOrigins = [
+    'https://web.sono.wtf',
+    'https://sono.wtf',
+    'https://www.sono.wtf'
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Password-Encrypted');
   res.header('Access-Control-Allow-Credentials', 'true');
